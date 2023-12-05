884842ccb013ab75a027047702e349c726eb5ef8
Vrushabh Pokale <vru.pokale@gmail.com>
2023-12-05 21:43:49 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StaffModule } from './staff/staff.module';
+import { StaffLoginComponent } from './staff/staff-login/staff-login.component';
+import { AdminModule } from './admin/admin.module';
+import { StudentModule } from './student/student.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StaffModule,
+    AdminModule,
+    StudentModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/student/student.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StudentDetailsComponent } from './student-details/student-details.component';
+
+
+
+@NgModule({
+  declarations: [
+    StudentDetailsComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    StudentDetailsComponent
+  ]
+})
+export class StudentModule { }
